b68efd5fcfffed5dcabe73fa1cb8adb5df9ac660
Jozsef Varga <jozsef.varga.123@gmail.com>
2019-04-05 13:13:42 +0200

    drawdiagonal added

--- week-01/day-3/DrawDiagonal/main.cpp
+++ week-01/day-3/DrawDiagonal/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-int main(int argc, char* args[]) {
+int main(int argc, char *args[]) {
 
     // Write a program that reads a number from the standard input, then draws a
     // square like this:
@@ -14,17 +14,32 @@ int main(int argc, char* args[]) {
     //
     // The square should have as many lines as the number was
 
-    int numlines;
+    int numlines = 0;
     std::cout << "Enter the number of lines" << std::endl;
     std::cin >> numlines;
 
-        for (int i=0; i < numlines; i++){
+    for (int i = 0; i < numlines; i++) {
+        std::cout << "%";
 
-
+    }
+    std::cout << std::endl;
+    for (int i = 0; i < numlines - 2; ++i) {
+        std::cout << "%";
+        for (int j = 0; j < numlines - 2; ++j) {
+            if (i == j) {
+                std::cout << "%";
+            } else {
+                std::cout << " " ;
+            }
 
         }
+        std::cout << "%" << std::endl;
+    }
 
 
+    for (int i = 0; i < numlines; i++) {
+        std::cout << "%";
+    }
 
     return 0;
 }

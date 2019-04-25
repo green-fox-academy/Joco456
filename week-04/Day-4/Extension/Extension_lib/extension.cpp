47cf8b75715e3de61f8630c7154958674503b2ce
Jozsef Varga <jozsef.varga.123@gmail.com>
2019-04-25 17:26:07 +0200

    Extension task almost ready, need work with it

---
+++ week-04/Day-4/Extension/Extension_lib/extension.cpp
@@ -0,0 +1,69 @@
+#include "extension.h"
+#include <algorithm>
+#include <sstream>
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int maxOfThree(int a, int b, int c)
+{
+    if (a > b && a > c)
+        return a;
+    else if (c > a && c > b)
+        return c;
+    else return b;
+}
+
+int median(const std::vector<int>& pool) {
+    std::sort(pool.begin(), pool.end());
+    return pool.at((pool.size())/2);
+}
+
+bool isVowel(char c) {
+    static const std::vector<char> vowels = {'a', 'u', 'o', 'e', 'i'};
+    return std::find(vowels.begin(), vowels.end(), c) != vowels.end();
+}
+
+std::vector<std::string> split(const std::string& str, char delim)
+{
+    std::stringstream ss(str);
+    std::string token;
+    std::vector<std::string> v;
+    while (std::getline(ss, token, delim)) {
+        v.push_back(token);
+    }
+
+    return v;
+}
+
+std::string join(const std::string& separator, const std::vector<std::string>& array)
+{
+    std::stringstream ss;
+    for(int i = 0; i < array.size(); ++i)
+    {
+        ss << array[i];
+        if(i != array.size() - 1)
+        {
+            ss << separator;
+        }
+    }
+
+    return ss.str();
+}
+
+std::string translate(const std::string& hungarian)
+{
+    std::string teve = hungarian;
+    int length = teve.size();
+    for (int i = 0; i < length; i++) {
+        const char c = teve.at(i);
+        if (isVowel(c)) {
+            teve = join(std::string(1, c) + "v" + std::string(1, c), split(teve, c));
+            i+=2;
+            length+=2;
+        }
+    }
+    return teve;
+}

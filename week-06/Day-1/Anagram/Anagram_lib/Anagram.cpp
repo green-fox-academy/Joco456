6eda46648a6526606a1b7de010ef51229b054279
Jozsef Varga <jozsef.varga.123@gmail.com>
2019-05-14 10:55:40 +0200

    Anagram task added

--- week-06/Day-1/Anagram/Anagram_lib/Anagram.cpp
+++ week-06/Day-1/Anagram/Anagram_lib/Anagram.cpp
@@ -1,4 +1,16 @@
 //
 // Created by Admin on 13/05/2019.
 //
+#include <iostream>
+#include <algorithm>
 
+bool isAnagram(std::string word1, std::string word2){
+
+    std::sort(word1.begin(), word1.end());
+    std::sort(word2.begin(), word2.end());
+
+    if (word1 == word2)
+        return true;
+    else
+        return false;
+}
--- week-06/Day-1/Anagram/Anagram_test/AnagramTest.cpp
+++ week-06/Day-1/Anagram/Anagram_test/AnagramTest.cpp
@@ -2,3 +2,31 @@
 // Created by Admin on 13/05/2019.
 //
 
+#include "gtest/gtest.h"
+#include "Anagram.cpp"
+
+TEST (AnagramaTest, testValidAnagram)
+{
+    //Arrange
+    std::string word1 = "gyomorrontas";
+    std::string word2 = "toronymorgas";
+
+    //Act
+    bool areTheyAnagrams = isAnagram(word1, word2);
+
+    //Assert
+    ASSERT_EQ(areTheyAnagrams, true);
+}
+
+TEST (AnagramaTest, testNotValidAnagram)
+{
+    //Arrange
+    std::string word1 = "kukasmelleny";
+    std::string word2 = "melleny";
+
+    //Act
+    bool areTheyAnagrams = isAnagram(word1, word2);
+
+    //Assert
+    ASSERT_EQ(areTheyAnagrams, false);
+}

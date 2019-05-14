b6082dea0b4826eb86d0e7f02d6257ede7e4edfa
Jozsef Varga <jozsef.varga.123@gmail.com>
2019-05-14 11:38:55 +0200

    Hello others task added

---
+++ week-06/Day-2/hello others/main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    printf("Hello, Zsofi! \n" "Hello, Donat!\n" "Hello, Bence!\n");
+
+    return 0;
+}

cfd6a8a61c6ac44cf77ec50f3129e1ba9953e37c
Jozsef Varga <jozsef.varga.123@gmail.com>
2019-05-14 16:40:04 +0200

    Distance task added

---
+++ week-06/Day-2/Distance/main.c
@@ -0,0 +1,35 @@
+
+#include <stdio.h>
+#include <string.h>
+
+// create a function which takes a char array as a parameter and
+// returns the distance between the first and last occurance of character 's'
+int distance(char *array);
+
+int main() {
+    char str[] = "This is a sample string";
+    char *first, *last;
+    printf("%d", distance(str));
+
+    return 0;
+}
+
+int distance(char *array) {
+
+    int firstIndex = -1;
+    int lastIndex = -1;
+
+    for (int i = 0; i < strlen(array); ++i) {
+        if (array[i] == 's') {
+            firstIndex = i;
+            break;
+        }
+    }
+    for (int j = 0; j < strlen(array); ++j) {
+        if (array[j] == 's') {
+            lastIndex = j;
+        }
+    }
+    return lastIndex - firstIndex;
+
+}

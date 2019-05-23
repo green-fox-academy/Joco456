fad35b7b232658e9f5d1ffd97770a4dd5e284830
Jozsef Varga <jozsef.varga.123@gmail.com>
2019-05-23 17:00:16 +0200

    test commit

---
+++ week-07/Day-4/Test_Project/src/main.c
@@ -0,0 +1,54 @@
+#include "stm32f7xx.h"
+#include "stm32746g_discovery.h"
+
+void init_led_pin()
+{
+	__HAL_RCC_GPIOF_CLK_ENABLE();
+
+	GPIO_InitTypeDef gpio_config;
+
+	gpio_config.Pin = GPIO_PIN_10 | GPIO_PIN_9 | GPIO_PIN_8 | GPIO_PIN_7;
+	gpio_config.Mode = GPIO_MODE_OUTPUT_PP;
+	gpio_config.Pull = GPIO_NOPULL;
+	gpio_config.Speed = GPIO_SPEED_FAST;
+
+	HAL_GPIO_Init(GPIOF, &gpio_config);
+}
+
+int main(void)
+{
+	HAL_Init();
+	init_led_pin();
+
+	while(1) {
+
+		for (int i = 0; i < 16; i++) {
+
+			if ((i % 2) > 0) {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_SET);
+			} else {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_7, GPIO_PIN_RESET);
+			}
+
+			if ((i % 4) > 1) {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_SET);
+			} else {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_8, GPIO_PIN_RESET);
+			}
+
+			if ((i % 8) > 3) {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_SET);
+			} else {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_9, GPIO_PIN_RESET);
+			}
+
+			if ((i % 16) > 7) {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_SET);
+			} else {
+				HAL_GPIO_WritePin(GPIOF, GPIO_PIN_10, GPIO_PIN_RESET);
+			}
+
+			HAL_Delay(1000);
+		}
+	}
+}
